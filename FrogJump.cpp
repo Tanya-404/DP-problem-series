49de47b1e4618ac7d4d5460859b3eaf47d636124
Tanya <tanyakoshta04@gmail.com>
2023-06-20 10:41:50 +0530

    saved

---
+++ FrogJump.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+#include <bits/stdc++.h> 
+#include<vector>
+int frogJump(int n, int *heights)
+{
+    int prev =0;
+    int prev2 = 0;
+    for(int i=1;i<n;i++)
+    {
+        int first = prev + abs(heights[i]-heights[i-1]);
+        int second=INT_MAX;
+        if(i>1) second = prev2 + abs(heights[i]-heights[i-2]);
+        int curr =min(first,second);
+        prev2= prev;
+        prev = curr;
+    }
+    return prev;
+}
+
+int main(){
+    // int heights[]={10 ,20, 30, 10};
+    int heights[]={7, 4 ,4, 2, 6, 6 ,3, 4 };
+    cout << frogJump(8,heights);
+    return 0;
+}
